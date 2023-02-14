ef5925738988f76680fb96a77bfaca958428bacc
Julio Francisco Zerpa Mayor <35926348+FranciscoZ360@users.noreply.github.com>
2023-02-14 13:06:22 -0300

    Add files via upload

---
+++ require/banner.php
@@ -0,0 +1,7 @@
+<font face="calibri"><table width="800" height="76" border="0" align="center" background="img/banner.png"  cellspacing="0" cellpadding="0">
+      <tr>
+        <td align="right" valign="bottom" height="20">Usuario: <?php  echo $_SESSION['usuario'];?> &nbsp;|&nbsp; Cambiar Contrase&ntilde;a<a href="cambiar_contrasena.php?id=<?php echo $_SESSION['id']; ?>"><img src="img/pass.png" alt="" width="20" height="20" border="0" /></a> |  Cerrar Sesi&oacute;n <a href="cerrarsesion.php"> <img src="img/off.png"  alt="Cerrar Sesion" width="20" height="20" border="0" /> </a></td>
+        
+      </tr>
+</table>
+</font>
--- require/menu.php
+++ require/menu.php
@@ -1,1 +1,64 @@
-
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryMenuBarVertical.css" rel="stylesheet" type="text/css" />
+<font face="calibri">
+<table cellpadding="0" cellspacing="0" border="0" width="19%">
+<tr> 
+<td align="left" width="143">
+        <ul id="MenuBar1" class="MenuBarVertical">        ---Men&uacute; Principal---       
+        <li ><a href="inicio.php" >Entrega|Prestamo</a>        </li>
+        <?php if ($_SESSION['nivel']==1) { ?>
+        <li><a href="#" class="MenuBarItemSubmenu">Cuenta de Usuario</a>
+          <ul>
+            <li><a href="agregar_usuario.php">Crear Nueva</a></li>
+            <li><a href="listar_usuario.php">Listado</a></li>
+          </ul>
+        </li>
+        <?php } ?>
+        <li><a href="#" class="MenuBarItemSubmenu">Profesor</a>
+          <ul>
+            <li><a href="agregar_profesor.php">Crear Nuevo</a></li>
+            <li><a href="listar_profesor.php">Listar</a></li>
+          </ul>
+        </li>
+        <li><a href="#" class="MenuBarItemSubmenu">Equipos</a>
+          <ul>
+            <li><a href="agregar_equipo.php">Agregar</a></li>
+            <li><a href="listar_equipo.php">Listar</a></li>
+            <li><a href="equipos_prestados.php">Equipos en pr&eacute;stamo</a></li>
+          </ul>
+        </li>
+        <li><a href="#" class="MenuBarItemSubmenu">Materiales</a>
+          <ul>
+            <li><a href="agregar_material.php">Agregar</a></li>
+            <li><a href="listar_material.php">Listar</a></li>
+          </ul>
+        </li>
+        <li><a href="#" class="MenuBarItemSubmenu">Reportes</a>
+          <ul>
+            <li><a href="#" class="MenuBarItemSubmenu">Prestamo equipos</a>
+              <ul>
+                <li><a href="reporte_fecha_equipo.php">Fecha</a></li>
+                <li><a href="reporte_profe_equipo.php">Profesor</a></li>
+              </ul>
+            </li>
+            <li><a href="#" class="MenuBarItemSubmenu">Entrega material</a>
+              <ul>
+                <li><a href="reporte_fecha_material.php">Fecha</a></li>
+                <li><a href="reporte_profe_material.php">Profesor</a></li>
+              </ul>
+            </li>
+            <?php if ($_SESSION['nivel']==1) { ?>
+            <li><a href="auditoria.php">Ver Auditoria</a></li><?php } ?>
+          </ul>
+        </li>
+        ---Men&uacute; Principal---
+        </ul>
+    </td>
+        </tr>
+        </table>
+        </font>
+    <script type="text/javascript">
+<!--
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgRight:"SpryAssets/SpryMenuBarRightHover.gif"});
+//-->
+  </script>
